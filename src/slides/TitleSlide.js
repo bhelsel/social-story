6f23689849dd606b4731b0db2d09c8b7dd7a6cbb
Brian Helsel <bhelsel@kumc.edu>
2025-07-30 13:07:45 -0500

    Update title slide to use css styling

--- src/slides/TitleSlide.js
+++ src/slides/TitleSlide.js
@@ -3,18 +3,10 @@ import classes from "./TitleSlide.module.css";
 
 function TitleSlide({ title, text, image }) {
   return (
-    <Slide
-      style={{ backgroundColor: "#f3f4f6", fontSize: "1.2rem" }}
-      textColor="#1f2937"
-      padding="1rem"
-    >
+    <Slide className={classes.slideStyling}>
       <div className={classes.titleSlide}>
         <h2>{title}</h2>
-        {text ? (
-          <p>{text}</p>
-        ) : (
-          <p>{`A Social Story for the ${title} study.`}</p>
-        )}
+        {text ? <p>{text}</p> : null}
         <img src={image} alt={title}></img>
       </div>
     </Slide>
@@ -22,3 +14,8 @@ function TitleSlide({ title, text, image }) {
 }
 
 export default TitleSlide;
+
+// style={{ backgroundColor: "#f3f4f6", fontSize: "1.2rem" }}
+// textColor="#1f2937"
+// padding="1rem"
+//className={classes.titleSlide}

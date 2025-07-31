99fdaa50c2c4984887ac1845eb7d3d9658ca1098
Brian Helsel <bhelsel@kumc.edu>
2025-07-31 16:19:18 -0500

    Remove modal from assessment component

--- src/components/Assessments.js
+++ src/components/Assessments.js
@@ -4,6 +4,7 @@ import studies from "../data/studies.json";
 import assessments from "../data/assessments.json";
 import classes from "./Assessments.module.css";
 import YouTubeEmbed from "./YouTubeEmbed.js";
+import AssessmentModal from "./AssessmentModal.js";
 
 function AssessmentPage() {
   const [studyAssessments, setStudyAssessments] = useState([]);
@@ -19,6 +20,16 @@ function AssessmentPage() {
       assessments.filter((x) => study.assessments.includes(x.id))
     );
   }, [study.assessments]);
+
+  const closeModal = () => {
+    setExpandedStudy(null);
+  };
+
+  const handleModalOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  };
 
   const toggleCardFlip = (cardId) => {
     setFlippedCards((prev) => {
@@ -45,19 +56,9 @@ function AssessmentPage() {
     }
   };
 
-  const handleExpandClick = (e, study) => {
+  const handleExpandClick = (e, assessment) => {
     e.stopPropagation();
-    setExpandedStudy(study);
-  };
-
-  const closeModal = () => {
-    setExpandedStudy(null);
-  };
-
-  const handleModalOverlayClick = (e) => {
-    if (e.target === e.currentTarget) {
-      closeModal();
-    }
+    setExpandedStudy(assessment);
   };
 
   return (
@@ -68,10 +69,10 @@ function AssessmentPage() {
           Click any card to learn more about what to expect.
         </p>
       </div>
-
       <div className={classes.assessmentsGrid}>
         {studyAssessments.map((study) => {
           const isFlipped = flippedCards.has(study.id);
+
           return (
             <div key={study.id} className={classes.cardContainer}>
               <div
@@ -150,7 +151,7 @@ function AssessmentPage() {
                       ].join(" ")}
                     >
                       <div className={classes.backHeader}>
-                        <h4 className={classes.backHeader}>What to Expect</h4>
+                        <h4>What to Expect</h4>
                         <button
                           className={classes.expandButton}
                           onClick={(e) => handleExpandClick(e, study)}
@@ -209,80 +210,13 @@ function AssessmentPage() {
 
       {/* Modal for expanded view */}
       {expandedStudy && (
-        <div className={classes.modalOverlay} onClick={handleModalOverlayClick}>
-          <div className={classes.modalContent}>
-            <div className={classes.modalHeader}>
-              <h2 className={classes.modalTitle}>
-                <span>{expandedStudy.icon}</span>
-                {expandedStudy.title}
-              </h2>
-              <button className={classes.modalCloseButton} onClick={closeModal}>
-                ×
-              </button>
-            </div>
-
-            <div className={classes.modalBody}>
-              <p className={classes.modalDescription}>
-                {expandedStudy.description}
-              </p>
-
-              <div className={classes.modalMeta}>
-                <div className={classes.modalMetaItem}>
-                  <div className={classes.durationDot}></div>
-                  <span className={classes.durationText}>
-                    {expandedStudy.duration}
-                  </span>
-                </div>
-                <div className={classes.modalMetaItem}>
-                  <div
-                    className={[
-                      classes.difficultyBadge,
-                      classes[getDifficultyClass(expandedStudy.difficulty)],
-                    ].join(" ")}
-                  >
-                    {expandedStudy.difficulty}
-                  </div>
-                </div>
-              </div>
-
-              <div className={classes.cardTags}>
-                {expandedStudy.tags.map((tag, index) => (
-                  <span key={index} className={classes.tag}>
-                    {tag}
-                  </span>
-                ))}
-              </div>
-
-              <h3 className={classes.modalProceduresTitle}>
-                Detailed Procedure Steps
-              </h3>
-
-              {expandedStudy.procedures.map((procedure, index) => (
-                <div key={index} className={classes.modalProcedureItem}>
-                  <div className={classes.modalProcedureHeader}>
-                    <span className={classes.modalProcedureIcon}>
-                      {procedure.icon}
-                    </span>
-                    <div className={classes.modalProcedureInfo}>
-                      <h4>{procedure.title}</h4>
-                      <span>{procedure.duration}</span>
-                    </div>
-                  </div>
-                  <p className={classes.modalProcedureDescription}>
-                    {procedure.description}
-                  </p>
-                  {procedure.details && (
-                    <ul className={classes.modalProcedureDetails}>
-                      {procedure.details.map((detail, detailIndex) => (
-                        <li key={detailIndex}>{detail}</li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
+        <AssessmentModal
+          expandedStudy={expandedStudy}
+          handleModalOverlayClick={handleModalOverlayClick}
+          closeModal={closeModal}
+          getDifficultyClass={getDifficultyClass}
+          variant="default"
+        />
       )}
     </div>
   );

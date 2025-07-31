dab05ead6e2645fc52e324349ce0dc24eac6dc5b
Brian Helsel <bhelsel@kumc.edu>
2025-07-31 16:19:38 -0500

    Add a Google map embed component

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,16 +1,20 @@
+import classes from "./GoogleMap.module.css";
+
 function GoogleMap({ location, link }) {
   return (
-    <>
-      <iframe
-        title={location}
-        src={link}
-        width="600"
-        height="450"
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-      ></iframe>
-    </>
+    <div className={classes.mapContainer}>
+      <div className={classes.mapWrapper}>
+        <iframe
+          title={location}
+          src={link}
+          width="600"
+          height="450"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
   );
 }
 export default GoogleMap;

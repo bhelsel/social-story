177d50a71078cfac50ef92517833198e99c14205
Brian Helsel <bhelsel@kumc.edu>
2025-08-18 19:54:53 -0500

    Update TitleSlide.js to use h3 for long titles

--- src/slides/TitleSlide.js
+++ src/slides/TitleSlide.js
@@ -5,7 +5,7 @@ function TitleSlide({ title, text, image }) {
   return (
     <Slide className={classes.slideStyling}>
       <div className={classes.titleSlide}>
-        <h2>{title}</h2>
+        {title.length < 20 ? <h2>{title}</h2> : <h3>{title}</h3>}
         {text ? <p>{text}</p> : null}
         <img src={image} alt={title}></img>
       </div>
@@ -14,8 +14,3 @@ function TitleSlide({ title, text, image }) {
 }
 
 export default TitleSlide;
-
-// style={{ backgroundColor: "#f3f4f6", fontSize: "1.2rem" }}
-// textColor="#1f2937"
-// padding="1rem"
-//className={classes.titleSlide}
